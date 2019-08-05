ed34983170d32faa9b532b8ecb42ee8077a25aa7
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-08-05 16:44:39 +0200

    to be c

---
+++ FromScratchWebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FromScratchWebApp.Controllers
+{
+    // In other parts of the code we will refer to this controller with just "Home" as its name
+    // (the controller part of the name will be automatecly added by MVC).
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();// defaults to looking for a view with the name of method name "Index"
+        }
+
+        public IActionResult Cars()
+        {
+            return View();
+        }
+    }
+}
--- FromScratchWebApp/Startup.cs
+++ FromScratchWebApp/Startup.cs
@@ -15,17 +15,23 @@ namespace FromScratchWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();              //Adds MVC to our project
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //The order of app.Use here is of importans as it determens i what order the requests are prossesed and in reversed order when it sends back the response
+            // this segmant is also referd to as Middlewere or pipeline
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles();           //Allowes os to use the files in the "wwwroot" folder
+
+            app.UseMvcWithDefaultRoute();   //Controller = Home & Action = Index
 
             //app.Run(async (context) =>
             //{
